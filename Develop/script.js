ae38dd369c956e041d679c3f5242fc46c905f42b
Kalena Almand <kalmand1057@gmail.com>
2020-08-04 20:12:12 -0500

    added confirm functions for lowercase uppercase numerics and special characters

--- Develop/script.js
+++ Develop/script.js
@@ -7,16 +7,25 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  var pswdLength = prompt("Password length must be between 8 and 128 characters.");
+
+  //Password Length
+  var pswdLength = prompt("Choose a password length between 8 and 128 characters.");
   
-//Password Length
   if (isNaN(pswdLength) || pswdLength < 8 || pswdLength > 128) {
     alert("Not enough/too much characters.");
-    while (isNaN(pswdLength) || pswdLength < 8 || pswdLength > 128) {
+  } while (isNaN(pswdLength) || pswdLength < 8 || pswdLength > 128) {
       pswdLength = prompt("Number of characters invalid. Please enter number of characters between 8 and 128.");
-    }
   } 
-  alert("Success!");
+  alert("Awesome!");
+  
+  //Confirms
+  var isLower = confirm("Would you like to add a lower case character?");
+  var isUpper = confirm("Would you like to add an upper case character?");
+  var isNumerics = confirm("Would you like to add a numeric?");
+  var isSpecial = confirm("Would you like to add a special character?");
+  if (isLower = false, isUpper = false, isNumerics = false, isSpecial = false); {
+    alert("You have to choose at least one!");
+  } 
 }
 
 // Add event listener to generate button
