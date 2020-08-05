634157837df7cbe3214f5e7dc30033bd34e86751
Kalena Almand <kalmand1057@gmail.com>
2020-08-04 19:18:14 -0500

    created prompt for password length

--- Develop/script.js
+++ Develop/script.js
@@ -3,11 +3,20 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  var pswdLength = prompt("Password length must be between 8 and 128 characters.");
+  
+//Password Length
+  if (isNaN(pswdLength) || pswdLength < 8 || pswdLength > 128) {
+    alert("Not enough/too much characters.");
+    while (isNaN(pswdLength) || pswdLength < 8 || pswdLength > 128) {
+      pswdLength = prompt("Number of characters invalid. Please enter number of characters between 8 and 128.");
+    }
+  } 
+  alert("Success!");
 }
 
 // Add event listener to generate button
