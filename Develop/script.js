ea61e64217e155e6615a32e67113addcb044318b
Kalena Almand <kalmand1057@gmail.com>
2020-08-08 20:23:41 -0500

    generating password with for loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
 
   //Password Length
   var pswdLength = prompt("Choose a password length between 8 and 128 characters.");
@@ -15,7 +14,7 @@ function writePassword() {
     pswdLength = prompt("Number of characters invalid. Please enter number of characters between 8 and 128.");
   } 
   alert("Awesome! You chose " + pswdLength + " characters!");
-  
+
   //Confirms
   var isLower = confirm("Would you like to add a lower case character?");
   var isUpper = confirm("Would you like to add an upper case character?");
@@ -30,29 +29,64 @@ function writePassword() {
     isSpecial = confirm("Would you like to add a special character?");
   }
   alert("Im out of the while loop!");
+
+  var password = generatePassword(pswdLength, isLower, isUpper, isNumerics, isSpecial);
+  passwordText.value = password;
 }
+
+  //Generating Password
+  function generatePassword(pswdLength, isLower, isUpper, isNumerics, isSpecial) {
+    var pswd = "";
+    for (var i = 0; i < pswdLength; i) {
+      if (isLower) {
+        pswd += randomLowercase();
+        i++;
+        if (i == pswdLength) {
+          break;
+        }
+      }
+      if (isUpper) {
+        pswd += randomUppercase();
+        i++;
+        if (i == pswdLength) {
+          break;
+        }
+      }
+      if (isNumerics) {
+        pswd += randomNumerics();
+        i++;
+        if (i == pswdLength) {
+          break;
+        }
+      }
+      if (isSpecial) {
+        pswd += randomSymbols();
+        i++;
+        if (i == pswdLength) {
+          break;
+        }
+      }
+    }
+    return pswd;
+  }
 
 //Generator Functions
 function randomLowercase() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-console.log(randomLowercase());
+//console.log(randomLowercase());
 
 function randomUppercase() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
-console.log(randomUppercase());
+//console.log(randomUppercase());
 
 function randomNumerics() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
-console.log(randomNumerics());
+//console.log(randomNumerics());
 
 function randomSymbols() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 33);
 }
-console.log(randomSymbols());
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//console.log(randomSymbols());
